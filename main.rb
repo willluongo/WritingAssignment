96e8b067743e3f8390dafd43a47e07cfaffe759b
Will Luongo <me@willluongo.com>
2010-06-21 01:57:18 -0400

    new file:   itemlist.yaml 	modified:   main.rb 	new file:   views/mainpage.haml

--- main.rb
+++ main.rb
@@ -1,8 +1,30 @@
 #WritingAssignment
 #A basic webapp that generates and tracks writing assignments for students
 
+require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'yaml'
 
 get '/' do
-    return "Greetings, planet."
+    @title = "WritingAssignments"
+    
+    bigarray = YAML.load_file( 'itemlist.yaml')
+    
+    random_one = rand(bigarray['objects'].length)
+    @object_one = bigarray['objects'][random_one]
+    
+    random_two = rand(bigarray['objects'].length)
+    @object_two = bigarray['objects'][random_two]
+    
+    random_three = rand(bigarray['objects'].length)
+    @object_three = bigarray['objects'][random_three]
+    
+    random_loc = rand(bigarray['locations'].length)
+    @location = bigarray['locations'][random_loc]
+    
+    random_dev = rand(bigarray['devices'].length)
+    @device = bigarray['devices'][random_dev]
+    
+    haml :mainpage
 end
