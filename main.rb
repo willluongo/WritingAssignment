406317a82f53df9e172eaf0dc9fb4c3af8a7b061
Will Luongo <me@willluongo.com>
2010-06-27 21:47:58 -0400

    deleted:    config.ru 	modified:   itemlist.yaml 	modified:   main.rb 	new file:   public/theme.css 	modified:   views/mainpage.haml

--- main.rb
+++ main.rb
@@ -5,17 +5,20 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'yaml'
+require 'sinatra/reloader'
 
 get '/' do
     @title = "WritingAssignments"
     
-    bigarray = YAML.load_file( 'itemlist.yaml')
+    bigarray = YAML.load_file('itemlist.yaml')
     
     random_one = rand(bigarray['objects'].length)
     @object_one = bigarray['objects'][random_one]
+    bigarray['objects'].delete_at(random_one)
     
     random_two = rand(bigarray['objects'].length)
     @object_two = bigarray['objects'][random_two]
+    bigarray['objects'].delete_at(random_two)
     
     random_three = rand(bigarray['objects'].length)
     @object_three = bigarray['objects'][random_three]
@@ -28,3 +31,9 @@ get '/' do
     
     haml :mainpage
 end
+
+post "/submitwork" do
+    return params[:work]
+end
+
+
